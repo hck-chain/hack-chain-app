c7b12f04bc83dc178a2252f8f19489625a44ae24
Emmanuel Pastor <juanemmanuelpd@gmail.com>
2026-02-11 12:18:56 -0600

    create educator

--- frontend/src/hooks/useEducatorRegistration.ts
+++ frontend/src/hooks/useEducatorRegistration.ts
@@ -55,14 +55,18 @@ export const useEducatorRegistration = () => {
                 throw new Error("MetaMask not detected");
             }
 
-            await window.ethereum.request({
-                method: "wallet_requestPermissions",
-                params: [{ eth_accounts: {} }],
-            });
+            // await window.ethereum.request({
+            //     method: "wallet_requestPermissions",
+            //     params: [{ eth_accounts: {} }],
+            // });
 
             const accounts = await window.ethereum.request({
                 method: "eth_requestAccounts",
             });
+
+            if (!accounts || accounts.length === 0) {
+                throw new Error("No account selected");
+            }
 
             const walletAddress = accounts[0];
             async function authorizeIssuer(walletAddress) {
@@ -86,7 +90,7 @@ export const useEducatorRegistration = () => {
                 return data;
             }
 
-            await authorizeIssuer(walletAddress); 
+            await authorizeIssuer(walletAddress);
             /////////////////////////////////////////////////////////////////////////////////////
 
             // Merge with wallet address and role
