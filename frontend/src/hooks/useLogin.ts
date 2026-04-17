f970d5ed48374460e204606d50aac5353125c936
Emmanuel Pastor <juanemmanuelpd@gmail.com>
2026-04-17 00:54:08 -0600

    login fixed

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,29 +1,45 @@
 import { useMutation } from "@tanstack/react-query";
 import { api } from "@/services/api";
+import { useNavigate } from "react-router-dom";s
 
 interface LoginCredentials {
-  email: string;
-  password: string;
+  wallet_address: string; 
+  role: string;          
 }
 
 interface LoginResponse {
   message: string;
   token: string;
-  expiresIn: string;
   user: {
     id: number;
-    email: string;
-    role: string;
-    name: string | null;
-    lastName: string | null;
-    walletAddress: string | null;
+    email: string | null;
+    role: string; 
+    wallet_address: string; 
   };
 }
 
 export const useLogin = () => {
+  const navigate = useNavigate();
+
   return useMutation<LoginResponse, Error, LoginCredentials>({
     mutationFn: async (credentials) => {
+      // Enviamos wallet_address y role
       return api.postPublic<LoginResponse>('/api/auth/login', credentials);
     },
+    onSuccess: (data) => {
+    
+      localStorage.setItem("token", data.token);
+
+      const role = data.user.role.toLowerCase();
+      
+      if (role === 'student') navigate('/dashboard-talent');
+      else if (role === 'issuer') navigate('/dashboard-issuer');
+      else if (role === 'recruiter') navigate('/dashboard-recruiter');
+      else navigate('/'); 
+    },
+    onError: (error) => {
+      console.error("Error en el login:", error);
+      alert("No se pudo iniciar sesión. Revisa tu wallet o rol.");
+    }
   });
 };
